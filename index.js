69b96e98c3cda6373ce23fa6248d9523f1252724
joaquin-espinosa733 <juaco.enero9@gmail.com>
2024-08-28 16:55:51 -0300

    index.html

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 import routes from "./src/routes/index.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Definir __dirname usando import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import connectDB from "./src/db/db.js";
 
@@ -15,10 +21,11 @@ connectDB();
 
 app.use("/api", routes);
 
+// Sirviendo el archivo HTML
 app.get("/", (req, res) => {
-  res.status(200).send("hola mundo");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(3000, () => {
-  console.log("server iniciado en el puesto http://localhost:3000");
+  console.log("server iniciado en el puerto http://localhost:3000;");
 });
